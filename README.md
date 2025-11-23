78c9eb846e01c184474b66b192ef72091e7bb559
Leon Tran <49257836+leontmd@users.noreply.github.com>
2025-11-23 15:53:46 -0800

    added Gradient Vector Flow

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 School project: convert raster image to mosaic image
+
+Gradient Vector Flow over raster image and equalized luminance image
+![alt text](GVF.png)
--- mosaic_main.cpp
+++ mosaic_main.cpp
@@ -11,15 +11,99 @@ using namespace std;
 void computeRobertsGradient(const Mat& grayEq, Mat& fx, Mat& fy)
 {
     Mat grayF;
+
     grayEq.convertTo(grayF, CV_32F, 1.0f / 255.0f);
 
-    Mat kx = (Mat_<float>(2, 2) << 1, 0,
-        0, -1);
-    Mat ky = (Mat_<float>(2, 2) << 0, 1,
-        -1, 0);
+    // Roberts kernel, kx is one diagonal and ky is the other
+    Mat kx = (Mat_<float>(2, 2) << 1, 0, 0, -1);
+    Mat ky = (Mat_<float>(2, 2) << 0, 1, -1, 0);
 
     filter2D(grayF, fx, CV_32F, kx, Point(0, 0), 0, BORDER_REPLICATE);
     filter2D(grayF, fy, CV_32F, ky, Point(0, 0), 0, BORDER_REPLICATE);
+}
+
+// --------------------------------------------------------
+// Gradient Vector Flow (GVF) computation
+// --------------------------------------------------------
+void computeGVF(const Mat& fx, const Mat& fy,
+    Mat& u, Mat& v,
+    float mu = 0.2f,
+    float dt = 0.1f,
+    int iterations = 100)
+{
+    CV_Assert(fx.type() == CV_32F && fy.type() == CV_32F);
+    CV_Assert(fx.size() == fy.size());
+
+    Mat mag, g2;
+    magnitude(fx, fy, mag);
+
+    //|∇f|^2
+    g2 = mag.mul(mag);
+
+    u = fx.clone();
+    v = fy.clone();
+
+    // Discrete 2D laplacian kernel
+    Mat laplacianKernel = (Mat_<float>(3, 3) <<
+        0, 1, 0,
+        1, -4, 1,
+        0, 1, 0
+        );
+
+    // laplacian of u and v
+    Mat lapU, lapV;
+    Mat tmpU, tmpV;
+
+    for (int it = 0; it < iterations; ++it)
+    {
+        filter2D(u, lapU, CV_32F, laplacianKernel, Point(-1, -1), 0, BORDER_REPLICATE);
+        filter2D(v, lapV, CV_32F, laplacianKernel, Point(-1, -1), 0, BORDER_REPLICATE);
+
+        tmpU = u - fx;
+        tmpU = mu * lapU - tmpU.mul(g2);
+
+        tmpV = v - fy;
+        tmpV = mu * lapV - tmpV.mul(g2);
+
+        u += dt * tmpU;
+        v += dt * tmpV;
+    }
+}
+
+// --------------------------------------------------------
+// Draw Gradient Flow Vector field
+// --------------------------------------------------------
+Mat drawGVFField(const Mat& image, const Mat& u, const Mat& v,
+    int step = 8, float scale = 5.0f, float magnitudeThreshold = 1e-3f)
+{
+    CV_Assert(u.type() == CV_32F && v.type() == CV_32F);
+    CV_Assert(u.size() == v.size());
+
+    Mat vis;
+    if (image.channels() == 1)
+        cvtColor(image, vis, COLOR_GRAY2BGR);
+    else
+        vis = image.clone();
+
+    for (int y = 0; y < vis.rows; y += step)
+    {
+        for (int x = 0; x < vis.cols; x += step)
+        {
+            float vx = u.at<float>(y, x);
+            float vy = v.at<float>(y, x);
+            float magnitude = std::sqrt(vx * vx + vy * vy);
+            if (magnitude < magnitudeThreshold) continue;
+
+            float nx = vx / magnitude;
+            float ny = vy / magnitude;
+
+            Point p0(x, y);
+            Point p1(x + (int)(scale * nx), y + (int)(scale * ny));
+
+            arrowedLine(vis, p0, p1, Scalar(255, 0, 0), 1, LINE_AA, 0, 0.3);
+        }
+    }
+    return vis;
 }
 
 int main() {
@@ -37,16 +121,20 @@ int main() {
     cvtColor(img, gray, COLOR_BGR2GRAY);    
 
     // Equalize(L(I))
-    Mat grayEq;
-    equalizeHist(gray, grayEq);
+    Mat equalizedLuminance;
+    equalizeHist(gray, equalizedLuminance);
 
     // Robert gradients(Equalize(L(I)))
     Mat fx, fy;
-    computeRobertsGradient(grayEq, fx, fy);
+    computeRobertsGradient(equalizedLuminance, fx, fy);
+
+    // Gradient Vector Flow (GVF)
+    Mat u, v;
+    computeGVF(fx, fy, u, v, 0.2f, 0.1f, 100);
 
     imshow("Original", img);
-    imshow("Gray", gray);
-    imshow("grayEq", grayEq);
+    imshow("GVF Field (arrows) raster image", drawGVFField(img, u, v));
+    imshow("GVF Field (arrows) equalized luminance", drawGVFField(equalizedLuminance, u, v));
 
     waitKey(0);
     return 0;
